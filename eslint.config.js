649f394742777331a5ae77749da70ff244e7cd07
chengpeiquan <chengpeiquan@chengpeiquan.com>
2023-08-20 11:49:12 +0800

    chore: typo

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 // @ts-check
 import { defineConfig, js, prettier } from '@bassist/eslint'
 
+// https://github.com/chengpeiquan/bassist/tree/main/packages/eslint
 export default defineConfig([
   ...prettier,
   ...js,
